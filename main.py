344c6bb9a654fd677b37a4b32fda6deab7c26ad1
Akhilk2012 <157044908+Akhilk2012@users.noreply.github.com>
2025-01-11 15:41:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,72 +21,60 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Firewall Frenzy")
 pygame.display.set_icon(pygame_icon)
 
-#FPS
-
-clock = pygame.time.Clock()
-FPS = 60
-
-#Player Movement
-
 moving_left = False
 moving_right = False
-
-#Colors
-
-BG = (50, 50, 50)
-
-def draw_bg():
-    screen.fill(BG)
 
 
 #Player Class
 
 class EthicalHacker(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, x, y, scale , speed):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('img/player/Idle/0.png')
+        self.speed = speed
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()  # Fixed this line
         self.rect.center = (x, y)
-
-    def move(self, moving_left, moving_right):
-        #Reset Movement
+    def move(self,moving_left,moving_right):
         dx = 0
         dy = 0
 
-        #Assigning movement variables for left and right
-
         if moving_left:
-                dx = -self.speed 
-                self.flip = True
-                self.direction = -1
-
-        if moving_right: 
+            dx = -self.speed
+        if moving_right:
             dx = self.speed
-            self.flip = False
-            self.direction = 1
-
-        #Rectangle Position
         self.rect.x += dx
-        self.rect.y += dy   
-
+        self.rect.y += dy
+            
+         
     def draw(self):
         screen.blit(self.image, self.rect)
 
-player = EthicalHacker(200, 200, 3)
-player2 = EthicalHacker(400, 200, 3)
-
-player.move(moving_left, moving_right)
+player = EthicalHacker(200, 200, 2 , 5)
+player2 = EthicalHacker(400, 200, 2 , 5)
 
 run = True
 while run:
     player.draw()
-    player2.draw()
-
+    player.move(moving_left,moving_right)
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True    
+            if event.key == pygame.K_ESCAPE:
+                run = False  
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False            
+           
 
     pygame.display.update()
 
