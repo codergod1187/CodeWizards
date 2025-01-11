65f185aa9fea3cee3ce351af8a3acc60547c2eda
Akhilk2012 <157044908+Akhilk2012@users.noreply.github.com>
2025-01-11 16:25:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Libraries
+#Libraries
 
 import pygame
 
@@ -21,17 +22,27 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Firewall Frenzy")
 pygame.display.set_icon(pygame_icon)
 
+clock = pygame.time.Clock()
+FPS = 60
+
 moving_left = False
 moving_right = False
-
-
+BG = (50,50,50)
+def draw_bg():
+    screen.fill(BG)
 #Player Class
 
 class EthicalHacker(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale , speed):
+    def __init__(self,char_type, x, y, scale , speed):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/player/Idle/0.png')
+        self.char_type = char_type
         self.speed = speed
+        self.direction = 1
+        self.flip = False
+        self.animation = []
+        self.index = 0
+        for i in range(5):
+            img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()  # Fixed this line
         self.rect.center = (x, y)
@@ -41,21 +52,28 @@ class EthicalHacker(pygame.sprite.Sprite):
 
         if moving_left:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = self.speed
+            self.flip = False
+            self.direction = 1
         self.rect.x += dx
         self.rect.y += dy
             
          
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip , False), self.rect)
 
-player = EthicalHacker(200, 200, 2 , 5)
-player2 = EthicalHacker(400, 200, 2 , 5)
+player = EthicalHacker('player',200, 200, 2 , 5)
+virus = EthicalHacker('virus',400, 200, 2 , 5)
 
 run = True
 while run:
+    clock.tick(FPS)
+    draw_bg()
     player.draw()
+    virus.draw()
     player.move(moving_left,moving_right)
     for event in pygame.event.get():
         #quit game
@@ -79,4 +97,3 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-
