b8a95cfa8b9bbb70c99816314c42de3eebfd4c1c
codergod1187 <128053412+codergod1187@users.noreply.github.com>
2025-01-11 14:29:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -21,6 +21,23 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Firewall Frenzy")
 pygame.display.set_icon(pygame_icon)
 
+#FPS
+
+clock = pygame.time.Clock()
+FPS = 60
+
+#Player Movement
+
+moving_left = False
+moving_right = False
+
+#Colors
+
+BG = (50, 50, 50)
+
+def draw_bg():
+    screen.fill(BG)
+
 
 #Player Class
 
@@ -32,11 +49,34 @@ class EthicalHacker(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()  # Fixed this line
         self.rect.center = (x, y)
 
+    def move(self, moving_left, moving_right):
+        #Reset Movement
+        dx = 0
+        dy = 0
+
+        #Assigning movement variables for left and right
+
+        if moving_left:
+                dx = -self.speed 
+                self.flip = True
+                self.direction = -1
+
+        if moving_right: 
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        #Rectangle Position
+        self.rect.x += dx
+        self.rect.y += dy   
+
     def draw(self):
         screen.blit(self.image, self.rect)
 
 player = EthicalHacker(200, 200, 3)
 player2 = EthicalHacker(400, 200, 3)
+
+player.move(moving_left, moving_right)
 
 run = True
 while run:
@@ -51,3 +91,4 @@ while run:
     pygame.display.update()
 
 pygame.quit()
+
